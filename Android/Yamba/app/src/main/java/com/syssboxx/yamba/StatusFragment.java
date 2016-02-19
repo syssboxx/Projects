3df869df8e49869bff7c0d5199c5c46f9cf1688a
syssboxx <syssboxx@gmail.com>
2016-02-19 14:45:13 +0200

    status layout changed to fragment

---
+++ Android/Yamba/app/src/main/java/com/syssboxx/yamba/StatusFragment.java
@@ -0,0 +1,128 @@
+package com.syssboxx.yamba;
+
+import android.app.Fragment;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClientException;
+
+/**
+ * Created by Stacy on 19.2.2016 г..
+ */
+public class StatusFragment extends Fragment implements View.OnClickListener {
+
+    private static final String TAG = "StatusFragment";
+
+    private Button buttonTweet;
+    private EditText editStatus;
+    private TextView textCount;
+
+    private int defaultTextColor;
+
+    final int MAX_COUNT = 140;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                            Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_status,container,false);
+
+        editStatus = (EditText) view.findViewById(R.id.editStatus);
+        textCount = (TextView) view.findViewById(R.id.textCount);
+        buttonTweet = (Button) view.findViewById(R.id.buttonTweet);
+
+        buttonTweet.setOnClickListener(this);
+
+        //get deafult color of the text according to the choosen them
+        defaultTextColor = textCount.getTextColors().getDefaultColor();
+
+        editStatus.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int count = MAX_COUNT - editStatus.length();
+                textCount.setText(Integer.toString(count));
+                textCount.setTextColor(defaultTextColor);
+
+                if(count < 10){
+                    textCount.setTextColor(Color.RED);
+                    if(count < 0){
+                        count = 0;
+                        textCount.setText(Integer.toString(count));
+                    }
+                }
+
+            }
+        });
+
+        return view;
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        String status = editStatus.getText().toString();
+        Log.d(TAG, status);
+        PostTask postTask = new PostTask();
+        postTask.execute(status);
+    }
+
+
+    private final class PostTask extends AsyncTask<String,Void,String> {
+
+        /**
+         * Override this method to perform a computation on a background thread. The
+         * specified parameters are the parameters passed to {@link #execute}
+         * by the caller of this task.
+         * <p/>
+         * This method can call {@link #publishProgress} to publish updates
+         * on the UI thread.
+         *
+         * @param params The parameters of the task.
+         * @return A result, defined by the subclass of this task.
+         * @see #onPreExecute()
+         * @see #onPostExecute
+         * @see #publishProgress
+         */
+        @Override
+        protected String doInBackground(String... params) {
+            String result;
+            YambaClient yambaCloud = new YambaClient("student","password");
+            try {
+                yambaCloud.postStatus(params[0]);
+                result = "Successfully posted";
+            } catch (YambaClientException e) {
+                e.printStackTrace();
+                result = "Failed to post to yamba service";
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            Toast.makeText(StatusFragment.this.getActivity(), result, Toast.LENGTH_LONG).show();
+        }
+    }
+}
